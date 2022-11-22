8e7effdcc6b0cd29b68dd205397272fbfe30a916
atifaziz1 <aatif.aziz1@gmail.com>
2022-11-22 14:30:14 +0300

    add console in src

--- src/index.tsx
+++ src/index.tsx
@@ -23,4 +23,7 @@
 import { NativeModules } from 'react-native';
 
 const { RNHijriDatePickerAndroid } = NativeModules;
+console.log('\n\n\n\n**************insideAPP2*****************\n\n\n\n\n');
+console.log(RNHijriDatePickerAndroid);
+console.log('\n\n\n\n***************insideAPP2****************\n\n\n\n\n');
 export default RNHijriDatePickerAndroid;
