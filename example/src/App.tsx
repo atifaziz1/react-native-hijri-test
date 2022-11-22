b19f4b2ea305dd259a3e1a7eabc1c0e79240423b
atifaziz1 <aatif.aziz1@gmail.com>
2022-11-22 14:05:06 +0300

    added console on export

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,29 +3,37 @@ import * as React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 // import { multiply } from 'react-native-hijri-test';
 
-export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+// export default function App() {
+//   const [result, setResult] = React.useState<number | undefined>();
 
-  // React.useEffect(() => {
-  //   multiply(3, 7).then(setResult);
-  // }, []);
+//   // React.useEffect(() => {
+//   //   multiply(3, 7).then(setResult);
+//   // }, []);
 
-  return (
-    <View style={styles.container}>
-      <Text>Result: 23</Text>
-    </View>
-  );
-}
+//   return (
+//     <View style={styles.container}>
+//       <Text>Result: 23</Text>
+//     </View>
+//   );
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   box: {
+//     width: 60,
+//     height: 60,
+//     marginVertical: 20,
+//   },
+// });
+
+import { NativeModules } from 'react-native';
+
+const { RNHijriDatePickerAndroid } = NativeModules;
+console.log('\n\n\n\n**************insideAPP2*****************\n\n\n\n\n');
+console.log(RNHijriDatePickerAndroid);
+console.log('\n\n\n\n***************insideAPP2****************\n\n\n\n\n');
+export default RNHijriDatePickerAndroid;
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import { NativeModules } from 'react-native';
 
 const { RNHijriDatePickerAndroid } = NativeModules;
+console.log('\n\n\n\n**************insideAPP1*****************\n\n\n\n\n');
+console.log(RNHijriDatePickerAndroid);
+console.log('\n\n\n\n***************insideAPP1****************\n\n\n\n\n');
 export default RNHijriDatePickerAndroid;
